9f6072342ccd4f77650d826bd3ffbb0afa5e9168
apple <appple@apples-MacBook-Pro.local>
2025-04-10 18:45:05 +0530

    status bar

--- App.tsx
+++ App.tsx
@@ -1,17 +1,19 @@
 
 
-import { StyleSheet, Text, View } from 'react-native'
+import { StatusBar, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { Provider } from 'react-redux'
 import store from './src/Redux/Store'
 import { NavigationContainer } from '@react-navigation/native'
 import LoginScreen from './src/Screens/Public/LoginScreen'
 import { AuthStack } from './src/Navigation/StackNavigation'
+import { WHITE } from './src/Components/Colors'
 
 const App = () => {
   return (
     <Provider store={store}>
       <NavigationContainer>
+        <StatusBar backgroundColor={WHITE}/>
        <AuthStack/>
       </NavigationContainer>
       </Provider>
