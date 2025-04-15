30cc983dbcd482aad4e7efc64925c39bd3bbe1db
apple <appple@apples-MacBook-Pro.local>
2025-04-15 14:50:48 +0530

    MESSAGE SCREEN

--- src/Screens/Private/Message.js
+++ src/Screens/Private/Message.js
@@ -1,11 +1,23 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import Header from '../../Components/Header'
+import CustomTextInput from '../../Components/CustomTextInput'
+import { FULL_WIDTH } from '../../Components/Typography'
 
 const Message = () => {
   return (
-    <View>
-      <Text>Message</Text>
-    </View>
+   <SafeAreaView style={{flex:1}}>
+
+       <Header title={'Messages'}/>
+       <View style={{width:FULL_WIDTH-50,alignSelf:"center",marginVertical:15}}>
+  <CustomTextInput searchIcon placeholder={'Search by name...'} inputStyle={{width:'90%'}} containerStyle={{width:'100%',borderRadius:30}}/>
+  </View>
+
+
+
+
+
+   </SafeAreaView>
   )
 }
 
